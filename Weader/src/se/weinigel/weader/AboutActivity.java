3c69eb01f76710ae4674001875e379c6eeb2ebe5
Christer Weinigel <christer@weinigel.se>
2013-10-24 15:57:16 +0200

    Add version to about activity

--- Weader/src/se/weinigel/weader/AboutActivity.java
+++ Weader/src/se/weinigel/weader/AboutActivity.java
@@ -3,6 +3,7 @@ package se.weinigel.weader;
 import se.weinigel.weader.R;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +15,21 @@ public class AboutActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_about);
+
+		String versionName;
+		try {
+			versionName = getPackageManager().getPackageInfo(getPackageName(),
+					0).versionName;
+		} catch (NameNotFoundException e) {
+			versionName = "unknown";
+		}
+		String versionString = getResources().getString(R.string.about_version, versionName);
+
+		TextView versionView = (TextView) findViewById(R.id.version);
+		if (versionView != null)
+			versionView.setText(versionString);
 
 		final TextView webSiteView = (TextView) findViewById(R.id.website);
 		webSiteView.setOnClickListener(new OnClickListener() {
