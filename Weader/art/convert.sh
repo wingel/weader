2162e982e6dcc03f2b995a2b8cc22eedfdbaa1ac
Christer Weinigel <christer@weinigel.se>
2013-10-25 11:05:09 +0200

    Convert menu icons from color (RGBA) to grayscale (LA)
    Ask convert to strip off any comments from the PNG files.
    It seems that the created/modified/software comments are
    still left, but some files did become slightly smaller.

--- Weader/art/convert.sh
+++ Weader/art/convert.sh
@@ -4,6 +4,22 @@ set -x
 cd `dirname $0`
 fns=
 fns="$fns ic_launcher"
+while read dir dpi; do
+    mkdir -p $dir
+    for fn in $fns; do
+	inkscape \
+	    --export-area-page --export-dpi=$dpi \
+	    --export-background='#000000' --export-background-opacity=0 \
+	    --export-png=tmp.png $fn.svg
+	convert tmp.png -strip $dir/$fn.png
+    done
+done <<EOF
+../res/drawable-ldpi 45
+../res/drawable-mdpi 60
+../res/drawable-hdpi 90
+../res/drawable-xhdpi 120
+EOF
+fns=
 fns="$fns ic_menu_mark_clear"
 fns="$fns ic_menu_mark_set"
 fns="$fns ic_menu_mark_all_clear"
@@ -14,7 +30,8 @@ while read dir dpi; do
 	inkscape \
 	    --export-area-page --export-dpi=$dpi \
 	    --export-background='#000000' --export-background-opacity=0 \
-	    --export-png=$dir/$fn.png $fn.svg
+	    --export-png=tmp.png $fn.svg
+	convert tmp.png -strip -channel RGBA -colorspace gray $dir/$fn.png
     done
 done <<EOF
 ../res/drawable-ldpi 45
@@ -22,3 +39,4 @@ done <<EOF
 ../res/drawable-hdpi 90
 ../res/drawable-xhdpi 120
 EOF
+rm -f tmp.png
